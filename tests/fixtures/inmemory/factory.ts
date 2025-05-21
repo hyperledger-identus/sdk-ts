8cab9192f8ccb58467d18ddc97432bcb7458d7d5
Francisco Javier Ribo Labrador <elribonazo@gmail.com>
2025-05-21 17:51:50 +0200

    fix: use make collections inside inmemory fixtures Signed-off-by: Francisco Javier Ribo Labrador <elribonazo@gmail.com>

--- tests/fixtures/inmemory/factory.ts
+++ tests/fixtures/inmemory/factory.ts
@@ -2,7 +2,7 @@ import { randomUUID } from "crypto";
 import { Apollo, Pluto } from "../../../src";
 import InMemoryStore from "./index";
 import { RxdbStore } from "@trust0/identus-store-rxdb";
-
+import { makeCollections } from "../../../src/pluto/collections";
 export const mockPluto = (args?: { apollo: Apollo; }) => {
   const apollo = args?.apollo ?? new Apollo();
 
@@ -10,7 +10,7 @@ export const mockPluto = (args?: { apollo: Apollo; }) => {
     name: 'test' + randomUUID(),
     storage: InMemoryStore,
     password: Buffer.from("demoapp").toString("hex")
-  });
+  }, makeCollections() as any);
 
   return new Pluto(store, apollo);
 };
