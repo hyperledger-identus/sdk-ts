45761a2b9a1670c47a0e8e0c761644aa107e9531
Allain Magyar <allain.magyar@iohk.io>
2025-06-25 09:10:15 -0300

    chore: update semantic-release scripts (#443)
    Signed-off-by: Allain Magyar <allain.magyar@iohk.io>

--- release.config.js
+++ release.config.js
@@ -1,20 +1,12 @@
-/**
- * @type {import('semantic-release').GlobalConfig}
- */
-module.exports = {
-  branches: [
-    'main',
-    '+([0-9])?(.{+([0-9]),x}).x',
-    { name: 'beta', prerelease: 'rc' } // make a branch 'beta' a pre-release branch as '6.6.0-rc1'
-  ],
-  plugins: [
-    ['@semantic-release/commit-analyzer', { preset: "conventionalcommits" }],
-    ['@semantic-release/release-notes-generator', { preset: "conventionalcommits" }],
+const { execSync } = require('node:child_process');
+
+const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
+const plugins = []
+if (branch === 'main') {
+  plugins.push(
     ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
-    ['@semantic-release/exec', { "prepareCmd": 'npm version ${nextRelease.version} --git-tag-version false' }],
-    ['@semantic-release/exec', { "prepareCmd": 'npm run build' }],
+    ['@semantic-release/release-notes-generator', { preset: "conventionalcommits" }],
     ['@semantic-release/exec', { "prepareCmd": 'npm run docs' }],
-    ['@semantic-release/exec', { "publishCmd": 'npm publish --tag ${nextRelease.channel || "latest"} --access public' }],
     ['@semantic-release/git', {
       assets: [
         'package.json',
@@ -23,6 +15,24 @@ module.exports = {
         'docs/**/*',
       ],
       message: 'chore(release): release ${nextRelease.version}\n\n${nextRelease.notes}',
-    }],
+    }]
+  )
+}
+
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+  branches: [
+    'main',
+    '+([0-9])?(.{+([0-9]),x}).x',
+    { name: 'beta', prerelease: 'rc' }
+  ],
+  plugins: [
+    ['@semantic-release/commit-analyzer', { preset: "conventionalcommits" }],
+    ['@semantic-release/exec', { "prepareCmd": 'npm version ${nextRelease.version} --git-tag-version false' }],
+    ['@semantic-release/exec', { "prepareCmd": 'npm run build' }],
+    ['@semantic-release/exec', { "publishCmd": 'npm publish --tag ${nextRelease.channel || "latest"} --access public' }],
+    ...plugins
   ],
 };
