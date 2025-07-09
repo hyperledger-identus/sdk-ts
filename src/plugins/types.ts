518bec7f42ffcf1b03a6c05b44fcc1eb59ea6873
Francisco Javier Ribo Labrador <elribonazo@gmail.com>
2025-07-09 12:47:23 +0200

    fix: type issue Signed-off-by: Francisco Javier Ribo Labrador <elribonazo@gmail.com>

--- src/plugins/types.ts
+++ src/plugins/types.ts
@@ -3,7 +3,7 @@ import type { Payload } from "../domain/protocols/Payload";
 import type { AgentContext } from "../edge-agent/Context";
 
 export namespace Plugins {
-  export abstract class Task<T = unknown> extends Utils.Task<Payload, T> {}
+  export abstract class Task<T = unknown, R = unknown> extends Utils.Task<Payload<R>, T> { }
 
   export type Context<T = Record<string, never>> = Utils.Task.Context<T & AgentContext>;
 }
