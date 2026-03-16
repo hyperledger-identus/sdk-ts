b3eb9f5b10a9b037c321a8684359ebf8ae4c173a
Javier Ribó <elribonazo@gmail.com>
2026-03-16 17:30:08 +0100

    fix: migrate towards maintained identus store package which does not … (#504)
    Signed-off-by: Francisco Javier Ribo Labrador <elribonazo@gmail.com>

--- integration-tests/e2e-tests/src/Utils.ts
+++ integration-tests/e2e-tests/src/Utils.ts
@@ -1,8 +1,70 @@
 import { appendFile, writeFileSync } from "fs"
+import { randomUUID } from "crypto";
+
 import crypto from "crypto"
 import { Buffer } from "buffer"
 
+import { RIDB, StorageType } from "@trust0/ridb";
+import { Property } from "@trust0/ridb-core";
+import { Apollo, makeCollections, Pluto } from "@hyperledger/identus-sdk";
+import { CollectionSchema, CreateInstanceArgs } from "./types";
+import { createStore } from "@trust0/identus-store";
+
+
+
+
 export class Utils {
+
+  static createPlutoInstance(args?: CreateInstanceArgs) {
+    const { collections = makeCollections() } = args ?? {};
+    const converted = Object.keys(collections).reduce<any>((all, current) => {
+      all.schemas[current] = this.migrateSchema(collections[current].schema);
+      all.migrations[current] = collections[current].migrationStrategies;
+      return all;
+    }, { schemas: {}, migrations: {} });
+
+    let db: RIDB<typeof converted['schemas']> = new RIDB({
+      dbName: "test-index" + randomUUID(),
+      ...converted
+    });
+
+    const apollo = args?.apollo ?? new Apollo();
+    const store = createStore({
+      db,
+      password: Buffer.from("demoapp").toString("hex"),
+      storageType: StorageType.InMemory,
+    });
+
+    return new Pluto(store, apollo)
+  }
+
+  private static migrateSchema<
+    T extends CollectionSchema,
+    P extends Record<string, Property>
+  >(schema: T, properties: P = {} as P): Omit<T, 'properties'> & {
+    properties: T['properties'] & P;
+    version: 0;
+  } {
+    const { properties: schemaProperties, ...schemaWithoutProperties } = schema;
+    return {
+      ...schemaWithoutProperties,
+      version: 0 as const,
+      properties: Object.fromEntries(
+        Object.entries({ ...schemaProperties, ...properties }).map(([key, value]) => {
+          const propValue: any = { ...value };
+          // Ensure required is explicitly set
+          if (propValue.required === undefined || propValue.required === false) {
+            propValue.required = false;
+          } else {
+            propValue.required = true;
+          }
+          propValue.maxLength = undefined;
+          return [key, propValue];
+        })
+      ) as T['properties'] & P
+    };
+  }
+
   static async asyncFilter<T>(arr: T[], predicate: (value: T, index: number, array: T[]) => Promise<boolean>) {
     const results = await Promise.all(arr.map(predicate))
     return arr.filter((_v, index) => results[index])
---
+++ integration-tests/e2e-tests/src/types.ts
@@ -0,0 +1,25 @@
+import { Apollo, makeCollections } from "@hyperledger/identus-sdk";
+
+export type MakeCollections = ReturnType<typeof makeCollections>
+
+export type Collections = {
+    [key in keyof MakeCollections]: MakeCollections[key]
+};
+
+export type CollectionSchemas = {
+    [key in keyof Collections]: {
+        version: Collections[key]['schema']['version'];
+        primaryKey: Collections[key]['schema']['primaryKey'];
+        type: Collections[key]['schema']['type'];
+        encrypted: Collections[key]['schema']['encrypted'];
+        properties: Collections[key]['schema']['properties'];
+    };
+}
+
+export type CollectionSchema = CollectionSchemas[keyof CollectionSchemas];
+
+export type CreateInstanceArgs = {
+    apollo?: Apollo;
+    name?: string;
+    collections?: any;
+}
