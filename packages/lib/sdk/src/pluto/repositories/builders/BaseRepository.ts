1b59e3ec6920677fdbeb842ca3596808e43fe538
Francisco Javier Ribo Labrador <elribonazo@gmail.com>
2026-03-22 07:30:03 +0100

    fix: code quality linter issue Signed-off-by: Francisco Javier Ribo Labrador <elribonazo@gmail.com>

--- packages/lib/sdk/src/pluto/repositories/builders/BaseRepository.ts
+++ packages/lib/sdk/src/pluto/repositories/builders/BaseRepository.ts
@@ -98,7 +98,7 @@ export abstract class BaseRepository<K extends TableName> {
   private async runQuery(query?: Query<CollectionSchemas[K]>): Promise<CollectionMap[K][]> {
     const mQuery = this.makeMangoQuery(query);
     try {
-      return this.store.query(this.name, mQuery);
+      return await this.store.query(this.name, mQuery);
     }
     catch {
       throw new Domain.PlutoError.StoreQueryFailed();
