93f9c2b1def7905c52380c96e788cb7b85f9bf5f
Zorslay#17 <145530795+Slambot01@users.noreply.github.com>
2026-04-07 15:29:28 +0530

    fix(build): updated dockerfile and readme for workspace structure (#531)
    Signed-off-by: Ritesh Pandit <riteshpandit1708@gmail.com>

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 # Navigate to the application directory
 cd /app
 
-# Install NPM dependencies
-npm install
+# Install dependencies using Yarn
+yarn install
 
 # Initialize Git repository if not present
 if [ ! -d ".git" ]; then
@@ -13,4 +13,4 @@ if [ ! -d ".git" ]; then
 fi
 
 # Run the build script
-npm run build
+yarn build
